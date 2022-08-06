5a58b30627ba0834034a6ec598a176afaa697768
geyi <geyichn@126.com>
2022-08-06 21:53:40 +0800

    缓存代码重构

--- base/src/main/java/com/airing/spring/cloud/base/cache/AbstractMapCache.java
+++ base/src/main/java/com/airing/spring/cloud/base/cache/AbstractMapCache.java
@@ -12,12 +12,12 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public abstract class AbstractMapCache<K, V> {
 
+    static final int HASH_BITS = 0x7fffffff; // usable bits of normal node hash
+
     // 指向堆内存内缓存数据的引用变量
     private Map<K, V> map = new ConcurrentHashMap<>();
     // 堆内存内缓存数据的版本
     private AtomicLong version = new AtomicLong(-1);
-    // 同步锁
-    private final Object lock = new Object();
     /*
     是否使用key的版本控制刷新缓存
     为true时必须重写getVersion(K key)方法
@@ -26,11 +26,27 @@ public abstract class AbstractMapCache<K, V> {
     // 保存每个key的版本
     private Map<K, Long> keyVersion = new ConcurrentHashMap<>();
 
+    private final Object[] locks = {
+            new Object(), new Object(), new Object(), new Object(),
+            new Object(), new Object(), new Object(), new Object(),
+            new Object(), new Object(), new Object(), new Object(),
+            new Object(), new Object(), new Object(), new Object(),
+            new Object(), new Object(), new Object(), new Object(),
+            new Object(), new Object(), new Object(), new Object(),
+            new Object(), new Object(), new Object(), new Object(),
+            new Object(), new Object(), new Object(), new Object()
+    };
+    private int lockCount = locks.length;
+
+    private VersionCtrl<K, V> versionCtrl;
+
     public AbstractMapCache() {
+        this.versionCtrl = new DefVersionCtrl();
     }
 
     public AbstractMapCache(boolean keyVersionCtrl) {
         this.keyVersionCtrl = keyVersionCtrl;
+        this.versionCtrl = new KeyVersionCtrl();
     }
 
     /**
@@ -48,51 +64,80 @@ public abstract class AbstractMapCache<K, V> {
 
     public abstract V load(K key);
 
-    private void cacheMap(K key) {
+    private V cacheMap(K key) {
         V v = this.load(key);
         if (v != null) {
             map.put(key, v);
         }
+        return v;
     }
 
     public V get(K key) {
-        long cacheVersion;
-        long currentVersion;
-        if (keyVersionCtrl) {
-            cacheVersion = this.getVersion(key);
-            currentVersion = keyVersion.getOrDefault(key, -1L);
-        } else {
-            cacheVersion = this.getVersion();
-            currentVersion = version.get();
-        }
-        if (currentVersion != cacheVersion) {
-            synchronized (lock) {
-                currentVersion = keyVersionCtrl ? keyVersion.getOrDefault(key, -1L) : version.get();
-                if (currentVersion != cacheVersion) {
-                    if (keyVersionCtrl) {
-                        keyVersion.put(key, cacheVersion);
-                        remove(key);
-                    } else {
-                        version.compareAndSet(currentVersion, cacheVersion);
-                        map.clear();
-                    }
-                    this.cacheMap(key);
-                }
-            }
-        }
-        V v;
-        if ((v = map.get(key)) == null) {
-            synchronized (lock) {
-                if ((v = map.get(key)) == null) {
-                    this.cacheMap(key);
-                }
-            }
-        }
-        return v;
+        return versionCtrl.get(key);
     }
 
     public V remove(K key) {
         return map.remove(key);
+    }
+
+    static final int spread(int h) {
+        return (h ^ (h >>> 16)) & HASH_BITS;
+    }
+
+    private final class KeyVersionCtrl implements VersionCtrl<K, V> {
+        @Override
+        public V get(K key) {
+            long cacheVersion = getVersion(key);
+            long currentVersion = keyVersion.getOrDefault(key, -1L);
+            if (currentVersion != cacheVersion) {
+                synchronized (locks[spread(key.hashCode()) & (lockCount - 1)]) {
+                    currentVersion = keyVersion.getOrDefault(key, -1L);
+                    if (currentVersion != cacheVersion) {
+                        keyVersion.put(key, cacheVersion);
+                        remove(key);
+                        return cacheMap(key);
+
+                    }
+                }
+            }
+            V v;
+            if ((v = map.get(key)) == null) {
+                synchronized (locks[spread(key.hashCode()) & (lockCount - 1)]) {
+                    if ((v = map.get(key)) == null) {
+                        v = cacheMap(key);
+                    }
+                }
+            }
+            return v;
+        }
+    }
+
+    private final class DefVersionCtrl implements VersionCtrl<K, V> {
+        @Override
+        public V get(K key) {
+            long cacheVersion = getVersion();
+            long currentVersion = version.get();
+            if (currentVersion != cacheVersion) {
+                synchronized (locks[0]) {
+                    currentVersion = version.get();
+                    if (currentVersion != cacheVersion) {
+                        version.compareAndSet(currentVersion, cacheVersion);
+                        map.clear();
+                        return cacheMap(key);
+
+                    }
+                }
+            }
+            V v;
+            if ((v = map.get(key)) == null) {
+                synchronized (locks[spread(key.hashCode()) & (lockCount - 1)]) {
+                    if ((v = map.get(key)) == null) {
+                        v = cacheMap(key);
+                    }
+                }
+            }
+            return v;
+        }
     }
 
 }
